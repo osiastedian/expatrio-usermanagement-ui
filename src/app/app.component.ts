920863d14123bef463f2df8140e5e432e908d792
Ted Ian Osias <ted.osias@trustarc.com>
2020-01-27 13:22:23 +0800

    Added OAUTH check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { AuthenticationService } from "./core/services/authentication.service";
+import { tap, switchMap } from "rxjs/operators";
+import { of, Observable, EMPTY } from "rxjs";
+import { Authentication } from "./models/authentication";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'usermanager';
+export class AppComponent implements OnInit {
+  title = "usermanager";
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private authenticationService: AuthenticationService
+  ) {}
+
+  ngOnInit(): void {
+    this.authenticate();
+  }
+
+  private authenticate() {
+    const regex = /code=(\w+)/g;
+    const results = regex.exec(window.location.href);
+    const code = results ? results[1] : null;
+    let authTokenObs: Observable<Authentication> = EMPTY;
+    if (!this.authenticationService.hasToken()) {
+      if (code) {
+        authTokenObs = this.authenticationService.fetchAccessToken(
+          code,
+          window.location.href
+        );
+      } else {
+        this.authenticationService.authenticationApp();
+      }
+    } else {
+      authTokenObs = of(this.authenticationService.getTokenInStorage());
+    }
+    authTokenObs.subscribe(auth => {
+      console.log("Authenticated!", auth);
+    });
+  }
 }
---
+++ src/app/core/services/authentication.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { UrlSegment } from "@angular/router";
+import { Authentication } from "src/app/models/authentication";
+import { tap } from "rxjs/operators";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root"
+})
+export class AuthenticationService {
+  private tokenStoreKey = "access_token";
+  private tokenStoreExpiration = "access_token_expiration";
+  private authServer = "http://localhost:8080/";
+  private clientId = "user-manager-ui";
+  private clientSecret = "user-manager-ui-secret";
+  constructor(private http: HttpClient) {}
+
+  authenticationApp() {
+    const grant_type = "authorization_code";
+    const response_type = "code";
+    const clientId = this.clientId;
+    const redirect_uri = window.location.href;
+    const authUrl = `${this.authServer}oauth/authorize?grant_type=${grant_type}&response_type=${response_type}&client_id=${clientId}&redirect_uri=${redirect_uri}`;
+    window.location.href = authUrl;
+  }
+
+  fetchAccessToken(
+    code: string,
+    redirect_uri: string
+  ): Observable<Authentication> {
+    const tokenUrl = `${this.authServer}oauth/token`;
+    const params = new HttpParams();
+    params
+      .set("grant_type", "authorization_code")
+      .set("code", code)
+      .set("redirect_uri", redirect_uri)
+      .set("client_id", this.clientId)
+      .set("client_secret", this.clientSecret);
+    return this.http
+      .post<Authentication>(tokenUrl, null, { params })
+      .pipe(
+        tap(auth => {
+          localStorage.setItem(this.tokenStoreKey, JSON.stringify(auth));
+          const today = new Date();
+          today.setTime(today.getSeconds() + auth.expires_in);
+          localStorage.setItem(this.tokenStoreExpiration, `${today.getTime()}`);
+        })
+      );
+  }
+
+  getTokenInStorage(): Authentication {
+    return JSON.parse(
+      localStorage.getItem(this.tokenStoreKey)
+    ) as Authentication;
+  }
+
+  hasToken(): boolean {
+    const token = localStorage.getItem(this.tokenStoreKey);
+    if (!token) {
+      return false;
+    }
+    const today = new Date();
+    const expirationDate = new Date(
+      localStorage.getItem(this.tokenStoreExpiration)
+    );
+    return expirationDate.getTime() > today.getTime();
+  }
+}
